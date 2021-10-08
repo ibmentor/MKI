5c75fad919bd26aff1435e522795a80b318231b5
Ibrahim Saifee <ibrahim.saifee@byjus.com>
2021-10-08 15:07:38 +0530

    Added Closed chats tab in mentor dashboard to list all the closed chats by the mentor.

--- client/sidebar/RoomList/RoomList.js
+++ client/sidebar/RoomList/RoomList.js
@@ -1,11 +1,13 @@
 import { Box } from '@rocket.chat/fuselage';
 import { useResizeObserver } from '@rocket.chat/fuselage-hooks';
+import { Meteor } from 'meteor/meteor';
 import React, { useRef, useEffect, useMemo } from 'react';
 import { Virtuoso } from 'react-virtuoso';
 
 import { useSession } from '../../contexts/SessionContext';
 import { useTranslation } from '../../contexts/TranslationContext';
 import { useUserPreference, useUserId } from '../../contexts/UserContext';
+import { useEndpointData } from '../../hooks/useEndpointData';
 import { useAvatarTemplate } from '../hooks/useAvatarTemplate';
 import { usePreventDefault } from '../hooks/usePreventDefault';
 import { useRoomList } from '../hooks/useRoomList';
@@ -30,7 +32,42 @@ const RoomList = () => {
 
 	const t = useTranslation();
 
-	const roomsList = useRoomList();
+	let roomsList = useRoomList();
+
+	const useQuery = ({ text, itemsPerPage, current }, [column, direction], userIdLoggedIn) =>
+		useMemo(
+			() => ({
+				sort: JSON.stringify({ [column]: direction === 'asc' ? 1 : -1 }),
+				open: false,
+				roomName: text,
+				agents: [userIdLoggedIn],
+				...(itemsPerPage && { count: itemsPerPage }),
+				...(current && { offset: current }),
+			}),
+			[column, current, direction, itemsPerPage, userIdLoggedIn, text],
+		);
+
+	const query = useQuery(
+		{ text: '', current: 0, itemsPerPage: 25 },
+		['closedAt', 'desc'],
+		Meteor.userId(),
+	);
+	const { value: closedRooms } = useEndpointData('livechat/rooms', query);
+	if (closedRooms) {
+		roomsList = [
+			...roomsList,
+			{
+				title: 'Closed chats',
+				data: closedRooms.rooms.map((room) => ({
+					...room,
+					rid: room._id,
+					_updatedAt: new Date(room._updatedAt),
+					lastMessage: { ...room.lastMessage, _updatedAt: new Date(room._updatedAt) },
+				})),
+			},
+		];
+	}
+
 	const itemData = useMemo(
 		() => ({
 			extended,
